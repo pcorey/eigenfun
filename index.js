736a2810d90465cfcd996e16ded8fa53af8d1b06
Pete Corey <petecorey@gmail.com>
2015-02-05 16:28:47 -0800

    Switched to math.js

--- index.js
+++ index.js
@@ -1,46 +1,16 @@
 var math = require('mathjs');
 
-var c = [0,0];
-
-function M2x2(value) {
-    if (value.constructor == Array) {
-        if (value[0].constructor != Array) {
-            this.buildFromComplex(value);
-        }
-        else {
-            this.value = value;
-        }
+function trace(m) {
+    var sum = 0;
+    for (var i = 0; i < m._data[0].length; i++) {
+        sum += m._data[i][i];
     }
+    return sum;
 }
 
-M2x2.prototype.buildFromComplex = function(complex) {
-    this.value = [
-        [complex[0], -complex[1]],
-        [complex[1],  complex[0]]
-    ];
-};
-
-M2x2.prototype.tr = function() {
-    return this.value[0][0] + this.value[1][1];
-};
-
-M2x2.prototype.det = function() {
-    var a = this.value[0][0];
-    var b = this.value[0][1];
-    var c = this.value[1][0];
-    var d = this.value[1][1];
-    return (a * d) - (c * b);
-};
-
-M2x2.prototype.toString = function() {
-    return '[' + this.value.map(function(row) {
-        return '['+row+']';
-    }).join(',') + ']';
-};
-
-M2x2.prototype.eigenvalues = function() {
-    var tr = this.tr();
-    var det = this.det();
+function eigenvalues(m) {
+    var tr = trace(m);
+    var det = math.det(m);
     var sqrt = math.chain(tr)
                    .multiply(tr)
                    .subtract(4*det)
@@ -50,9 +20,10 @@ M2x2.prototype.eigenvalues = function() {
         math.chain(tr).add(sqrt).divide(2).done(),
         math.chain(tr).subtract(sqrt).divide(2).done()
     ];
-};
+}
 
-//var m = new M2x2([1,2]);
-var m = new M2x2([1,2]);
-console.log(m+'');
-console.log(m.eigenvalues());
+var c = math.complex(1,2);
+var m = math.matrix([[c.re, -c.im],
+                     [c.im,  c.re]]);
+
+console.log(eigenvalues(m));
